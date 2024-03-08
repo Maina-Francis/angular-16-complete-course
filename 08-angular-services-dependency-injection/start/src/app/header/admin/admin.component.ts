c71e8aaa7e482665901d7b36ec6f2633596d3ab5
Francis <francismainakabutha@gmail.com>
2024-03-08 07:02:12 +0300

    created an instance of the UserService in the app module

--- 08-angular-services-dependency-injection/start/src/app/header/admin/admin.component.ts
+++ 08-angular-services-dependency-injection/start/src/app/header/admin/admin.component.ts
@@ -1,13 +1,24 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject } from "@angular/core";
+import { UserService } from "src/app/services/user.service";
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html'
+  selector: "app-admin",
+  templateUrl: "./admin.component.html",
 })
 export class AdminComponent {
+  constructor(private userService: UserService) {}
 
-  name: string = '';
-  gender: string = 'Male';
-  subType: string = 'Yearly';
-  status: string = 'Active';
+  name: string = "";
+  gender: string = "Male";
+  status: string = "Active";
+  subType: string = "Yearly";
+
+  createNewUser() {
+    this.userService.createUser(
+      this.name,
+      this.gender,
+      this.subType,
+      this.status
+    );
+  }
 }
